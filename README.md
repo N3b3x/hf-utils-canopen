b49a13021803e5ce7663d288c2b3b34031a2dfc7
Nebiyu Tadesse <nebysma@gmail.com>
2025-06-09 21:56:56 -0600

    Add generic CAN frame and DS402 constants

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # HF-CANOPEN
-CANopen interface - as used in the HardFOC controller
+
+A collection of lightweight helpers for building CANopen frames used with motor
+drivers.  The code is completely self contained and includes a minimal
+`CanFrame` structure so it can be integrated without external
+dependencies.
+
+## Features
+
+- Construction of standard NMT and expedited SDO frames
+- Utilities for CiA DS402 drive control objects
+- Support for BLDC, stepper and DC motor controllers
+
+These helpers can be used on bare-metal or hosted systems to compose CANopen
+messages for controlling motion hardware.
--- include/CanOpenMotorUtils.h
+++ include/CanOpenMotorUtils.h
@@ -10,11 +10,41 @@ namespace CanOpen {
  * @brief Operation modes for drive control.
  */
 enum class OperationMode : int8_t {
-    ProfilePosition = 1, ///< Profile position mode
-    ProfileVelocity = 3, ///< Profile velocity mode
-    Homing          = 6, ///< Homing mode
-    CyclicPosition  = 8, ///< Cyclic synchronous position mode
-    CyclicVelocity  = 9  ///< Cyclic synchronous velocity mode
+    ProfilePosition     = 1,  ///< Profile position mode
+    ProfileVelocity     = 3,  ///< Profile velocity mode
+    ProfileTorque       = 4,  ///< Profile torque mode
+    Homing              = 6,  ///< Homing mode
+    InterpolatedPosition = 7, ///< Interpolated position mode
+    CyclicPosition      = 8,  ///< Cyclic synchronous position mode
+    CyclicVelocity      = 9,  ///< Cyclic synchronous velocity mode
+    CyclicTorque        = 10  ///< Cyclic synchronous torque mode
+};
+
+/**
+ * @brief Common CiA DS402 drive states encoded in the status word.
+ */
+enum class DriveState : uint16_t {
+    NotReadyToSwitchOn  = 0x0000,
+    SwitchOnDisabled    = 0x0040,
+    ReadyToSwitchOn     = 0x0021,
+    SwitchedOn          = 0x0023,
+    OperationEnabled    = 0x0027,
+    QuickStopActive     = 0x0007,
+    FaultReactionActive = 0x000F,
+    Fault               = 0x0008
+};
+
+/**
+ * @brief Helper values for common control word commands.
+ */
+enum class ControlWordCommand : uint16_t {
+    Shutdown        = 0x0006,
+    SwitchOn        = 0x0007,
+    DisableVoltage  = 0x0000,
+    QuickStop       = 0x0002,
+    DisableOperation = 0x0007,
+    EnableOperation = 0x000F,
+    FaultReset      = 0x0080
 };
 
 /**
@@ -24,8 +54,8 @@ enum class OperationMode : int8_t {
  * @param subIndex Sub-index
  * @return Populated CAN frame
  */
-FlexCan::Frame BuildSdoUpload(uint8_t nodeId, uint16_t index,
-                              uint8_t subIndex) noexcept;
+CanFrame BuildSdoUpload(uint8_t nodeId, uint16_t index,
+                        uint8_t subIndex) noexcept;
 
 /**
  * @brief Build a heartbeat frame.
@@ -33,7 +63,7 @@ FlexCan::Frame BuildSdoUpload(uint8_t nodeId, uint16_t index,
  * @param state Node state encoded in the heartbeat
  * @return Populated CAN frame
  */
-FlexCan::Frame BuildHeartbeat(uint8_t nodeId, uint8_t state) noexcept;
+CanFrame BuildHeartbeat(uint8_t nodeId, uint8_t state) noexcept;
 
 /**
  * @brief Build a control word download frame (index 0x6040).
@@ -41,7 +71,7 @@ FlexCan::Frame BuildHeartbeat(uint8_t nodeId, uint8_t state) noexcept;
  * @param controlWord Control word value
  * @return Populated CAN frame
  */
-FlexCan::Frame BuildControlWord(uint8_t nodeId, uint16_t controlWord) noexcept;
+CanFrame BuildControlWord(uint8_t nodeId, uint16_t controlWord) noexcept;
 
 /**
  * @brief Build a mode of operation download frame (index 0x6060).
@@ -49,7 +79,7 @@ FlexCan::Frame BuildControlWord(uint8_t nodeId, uint16_t controlWord) noexcept;
  * @param mode Desired operation mode
  * @return Populated CAN frame
  */
-FlexCan::Frame BuildModeOfOperation(uint8_t nodeId, OperationMode mode) noexcept;
+CanFrame BuildModeOfOperation(uint8_t nodeId, OperationMode mode) noexcept;
 
 /**
  * @brief Build a target velocity download frame (index 0x60FF).
@@ -57,7 +87,7 @@ FlexCan::Frame BuildModeOfOperation(uint8_t nodeId, OperationMode mode) noexcept
  * @param velocity Target velocity
  * @return Populated CAN frame
  */
-FlexCan::Frame BuildTargetVelocity(uint8_t nodeId, int32_t velocity) noexcept;
+CanFrame BuildTargetVelocity(uint8_t nodeId, int32_t velocity) noexcept;
 
 /**
  * @brief Build a target position download frame (index 0x607A).
@@ -65,7 +95,47 @@ FlexCan::Frame BuildTargetVelocity(uint8_t nodeId, int32_t velocity) noexcept;
  * @param position Target position
  * @return Populated CAN frame
  */
-FlexCan::Frame BuildTargetPosition(uint8_t nodeId, int32_t position) noexcept;
+CanFrame BuildTargetPosition(uint8_t nodeId, int32_t position) noexcept;
+
+/**
+ * @brief Request the status word (index 0x6041).
+ */
+CanFrame BuildStatusWordRequest(uint8_t nodeId) noexcept;
+
+/**
+ * @brief Request the actual position (index 0x6064).
+ */
+CanFrame BuildActualPositionRequest(uint8_t nodeId) noexcept;
+
+/**
+ * @brief Request the actual velocity (index 0x606C).
+ */
+CanFrame BuildActualVelocityRequest(uint8_t nodeId) noexcept;
+
+/**
+ * @brief Build a target torque download frame (index 0x6071).
+ */
+CanFrame BuildTargetTorque(uint8_t nodeId, int16_t torque) noexcept;
+
+/**
+ * @brief Build a target acceleration download frame (index 0x60C5).
+ */
+CanFrame BuildTargetAcceleration(uint8_t nodeId, int32_t acceleration) noexcept;
+
+/**
+ * @brief Build a max profile velocity download frame (index 0x607F).
+ */
+CanFrame BuildMaxProfileVelocity(uint8_t nodeId, uint32_t velocity) noexcept;
+
+/**
+ * @brief Build a profile acceleration download frame (index 0x6083).
+ */
+CanFrame BuildProfileAcceleration(uint8_t nodeId, uint32_t accel) noexcept;
+
+/**
+ * @brief Build a profile deceleration download frame (index 0x6084).
+ */
+CanFrame BuildProfileDeceleration(uint8_t nodeId, uint32_t decel) noexcept;
 
 } // namespace CanOpen
 
